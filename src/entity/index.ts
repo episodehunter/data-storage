2c4babcc82036a8a1b7dda7eed1d12fa9e82c283
tjoskar <kontakta@oskarkarlsson.nu>
2017-09-24 15:03:59 +0200

    ✨  Add system info

--- src/entity/index.ts
+++ src/entity/index.ts
@@ -1,3 +1,4 @@
 export { TvWatched } from './tv-watched';
 export { Show } from './show';
 export { Episode } from './episode';
+export { SystemInfo } from './system-info';
---
+++ src/entity/system-info.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('system_setings')
+export class SystemInfo {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('string', { length: 11 })
+    key: string;
+
+    @Column('string', { length: 255 })
+    value: string;
+
+}
