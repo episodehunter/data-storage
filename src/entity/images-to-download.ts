12e26c9ae55459c98e221e151321e9f3865b5ce3
tjoskar <kontakta@oskarkarlsson.nu>
2017-10-24 11:56:45 +0200

    ✨ Add entity for images to download

---
+++ src/entity/images-to-download.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('images_download')
+export class imagesToDownload {
+  @PrimaryGeneratedColumn() id: number;
+
+  @Column('int', { length: 10 })
+  media_id: number;
+
+  @Column('string', { length: 11 })
+  type: string;
+
+  @Column('string', { length: 255 })
+  path: string;
+}
--- src/entity/index.ts
+++ src/entity/index.ts
@@ -2,3 +2,4 @@ export { TvWatched } from './tv-watched';
 export { Show } from './show';
 export { Episode } from './episode';
 export { SystemInfo } from './system-info';
+import { imagesToDownload } from './images-to-download';
